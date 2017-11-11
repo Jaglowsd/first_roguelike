9e155e5090efa22e730d02f983c2102d52495af6
jaglowsd <david.jaglowski@lawrence.edu>
2017-11-10 22:27:39 -0600

    Monsters now have loot tables tied to their fighter definition.

--- fighter/fighter_definitions.py
+++ fighter/fighter_definitions.py
@@ -15,29 +15,33 @@ player =	   {
 					1, 1, 1], # Base stats
 					'object': ['Player', '@', libtcod.white, True]}
 hollow = 	   {
-					'fighter_comp': [35, 1, 92, None, None, None,
+					'fighter_comp': [35, 1, 92, None, 'hollow', None,
 					1, 0, 0, 0,
 					4, 0, 0, 0,
 					1, 1, 1],
-					'object': ['Hollow', 'h', libtcod.light_fuchsia, True]}
+					'object': ['Hollow', 'h', libtcod.light_fuchsia, True],
+					'loot': {'nothing': 76, 'broken_str_sword': 14, 'soul_of_a_lost_undead': 10}}
 undead_rat = 	{
-					'fighter_comp': [15, 1, 60, None, None, None,
+					'fighter_comp': [15, 1, 60, None, 'undead_rat', None,
 					0, 0, 0, 0,
 					3, 0, 0, 0,
 					1, 1, 1],
-					'object': ['Rat', 'r', libtcod.dark_sepia, True]}
+					'object': ['Rat', 'r', libtcod.dark_sepia, True],
+					'loot': {'nothing': 95, 'soul_of_a_lost_undead': 5}}
 undead_soldier= {
-					'fighter_comp': [50, 1, 146, None, None, None,
+					'fighter_comp': [50, 1, 232, None, 'undead_soldier', None,
 					4, 0, 0, 0,
 					8, 0, 0, 0,
 					1, 1, 1],
-					'object': ['Undead Soldier', 's', libtcod.silver, True]}
+					'object': ['Undead Soldier', 's', libtcod.silver, True],
+					'loot': {'nothing': 80, 'wooden_shield': 20}}
 black_knight_sword={
-					'fighter_comp': [50, 1, 450, None, None, None,
+					'fighter_comp': [50, 1, 450, None, 'black_knight_sword', None,
 					7, 3, 1, 1,
-					10, 0, 0, 0,
+					4, 4, 0, 0,
 					1, 1, 1],
-					'object': ['Black Knight', 'B', libtcod.black, True]}
+					'object': ['Black Knight', 'B', libtcod.black, True],
+					'loot': {'nothing': 95, 'black_knight_greatsword': 5}}
 asylum_demon = 	   {
 					'fighter_comp': [100, 1, 5000, None, None, None,
 					6, 0, 0, 0,
